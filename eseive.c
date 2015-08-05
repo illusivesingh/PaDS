2ba4c6eb08ec0cb288d7479685cbc414083637c8
illusivesingh <illusivesingh@gmail.com>
2015-08-05 15:03:31 +0530

    All Things done, only learn the seiving algorithm and implement it.

--- eseive.c
+++ eseive.c
@@ -8,81 +8,23 @@
 
 typedef long long int big;
 
-extern int isPrime(big);
-
-void eseive(const big num,
-	char * arr);
-
-void printPrimes(const big num,
-	const char * arr);
-
-void checkAccuracy(const big num, 
-		const int numThreads);
-
-void checkPerformance(const big num,
-		const int numThreads);
-
-double getWallTime()
-{
-    struct timeval time;
-    if(gettimeofday(&time, NULL))
-	return 0;
-    double wallTime = (double)time.tv_sec + (double)time.tv_usec * 0.000001;
-    return wallTime;
-}
-
-/* Input:
- *  bits: Number of bits in the random number that will be genrated.
- * Output:
- *  A random number having "bits" number of bits.
- * This Function:
- *  For each bit:
- *   1   genrates a random number.
- *   2   takes its modulus with 2 to get 0 or 1, randomly.
- *   3   left shifts the number, num, (the random number genrated so far
- *       by 1.
- *   4   Then sets the Right most digit as 0 or 1 based on the above
- *       randomly genrated bit.
- * */
-big createBigNumber (const int bits)
-{
-    big num = 0; // Initialize all bits to 0.
-    big thisBit; // Temporary variable used to store a bit in loop.
-
-    srand(time(NULL)); // Seed for rand ().
-    
-    // For each bit.
-    for(int i = 0; i < bits; ++i)
-    {
-        // Gerate a random bit (%2) and sdet it to the ith bit.
-        num     = num << 1; // left shift by 1.
-        
-        thisBit = (rand() % 2); /* Randomly get 0 or 1. All other bits
-            * except the least significant digit in thisBit are 0. 
-            * The least significant is either 0 or 1, randomly*/
- 
-        num     |= thisBit; // Binary OR operation with thisBit
-    }
-    return num;
-}
-
-char * createArray(const big num)
-{
-    char * arr = (char *)malloc(num * sizeof(char));
-    memset(arr, 0, num * sizeof(char));
-    return arr;
-}
-
-void destroyArray(char * arr)
-{
-    if(arr)
-	free(arr);
-    arr = NULL;
-}
-
-
 int main(int argc, char ** argv)
 {
+    /***************** Function Declarations ***********************/
+    
+    // Costructs a random number having "bits" number of bits.
+    big createBigNumber (const int bits);
+
+    // Invokes the esieve routine ane prints its accuarray stat.
+    void checkAccuracy (const big num, const int numThreads);
+
+    // Invokes the esieve routine and prints its performance stat.
+    void checkPerformance (const big num, const int numThreads);
+    
+    /***************************************************************/
+
+
+
     if (argc != 5)
     {
         // Number of arguments is not correct.
@@ -137,13 +79,13 @@ int main(int argc, char ** argv)
     {
         // Evaluate Accuracy
         printf ("Evaluating for Accuracy.\n");
-        // checkAccuracy(num, numThreads);
+        checkAccuracy(num, numThreads);
     }
     else if(checkPerf != 0)
     {
         // Evaluate performance
         printf ("Evaluating for Performance.\n");
-        // checkPerformance(num, numThreads);
+        checkPerformance(num, numThreads);
     }
     else
     {
@@ -154,65 +96,270 @@ int main(int argc, char ** argv)
     return 0;
 }
 
-void eseive(const big num,
-	char * arr)
+
+/* Input:
+ *      num: A random number for which the performance will be evaluated.
+ *      numThreads: number of threads to measure the accuarcy with.
+ *  Output: 
+ *      Prints the Perfromance measure to stdout.
+ *  This Function:
+ * */
+void eseive (const big num, char * arr)
 {
+    if (arr == NULL)
+    {
+        printf ("eseive: array pointer is NULL.\n");
+        printf ("Fatal Error. Exiting.\n");
+        exit (1);
+    }
+    // else continue execution.
 
-//Fill in the Code
-
+    // Executing the seiving algorithm in parallel.
+    /*
+#pragma omp parallel for
+    for (big i = 0; i < num; ++i)
+    {
+        // write the seiving algo.
+    }*/
 }
 
 
+/* Input:
+ *      num: A random number for which the performance will be evaluated.
+ *      numThreads: number of threads to measure the accuarcy with.
+ *  Output: 
+ *      Prints the Perfromance measure to stdout.
+ *  This Function:
+ *      For each number between 3 to n.
+ *      compares the flag in about number being prime / not prime in
+ *      the array "arr" to the number actually being prime / not prime.
+ *      and prints the numbers for which the the prime / not prime flag
+ *      doesn't agree.
+ * */
 void checkAccuracy (const big num, const int numThreads)
 {
-    char * arr = createArray(num);
-    omp_set_num_threads(numThreads);
-    eseive(num, arr);
+    /***************** Function Declarations ***********************/
+    
+    // Implements the seiving algorithm
+    void eseive (const big num, char * arr);
+
+    // Allocates an empty array of size num and initializes it to 0s
+    char *createArray (const big num);
+
+    // Deallocates the array pointed by arr.
+    void destroyArray (char * arr);
+
+    // Returns 0 if input number is not prime, else returns non zero num.
+    extern int isPrime (big);
+
+    /***************************************************************/
+    char * arr = createArray(num); /* creates an array of size num
+        * and initializes each element to 0. */
+    
+    omp_set_num_threads(numThreads); // set number of threads.
+    
+    eseive(num, arr); // run the algorithm.
+    
     //printPrimes(num, arr);
-    for(big i = 3; i < num; ++i)
+    // For each number from i = 3 to n.
+    for (big i = 3; i < num; ++i)
     {
-	if(!arr[i])
-	{
-	    if(!isPrime(i))
-		printf("VIOLATION : %lld is not prime\n", i);
-	}
-	else
-	{
-	    if(isPrime(i))
-		printf("VIOLATION : %lld is prime\n", i);
-	}
+        // Check if the number is marked as prime or not.
+        if (arr[i] != 0)
+        {
+            // The number i is marked as prime.
+            if (isPrime (i) == 0)
+                printf("VIOLATION : %lld is not prime\n", i); /* but 
+                    * it is not actually a prime number. */
+            else
+                ; // And "i" is a prime number, correct result.
+        }
+        else // arr [i] == 0;
+        {
+            // The number "i" is marked as composite.
+            if (isPrime (i) != 0)
+                printf("VIOLATION : %lld is prime\n", i); /* but it is 
+                    * actually a prime number. */
+            else
+                ; // And "i" is not a prime, correct result.
+        }
     }
-    destroyArray(arr);
+    
+    destroyArray(arr); // deallocate the array space.
     return;
 }
 
-void checkPerformance(const big num,
-		const int numThreads)
+
+/* Input:
+ *      num: A random number for which the performance will be evaluated.
+ *      numThreads: number of threads to measure the performance.
+ *  Output: 
+ *      Prints the Perfromance measure to stdout.
+ *  This Function:
+ *   1  sets the prime / not prime flag for each number i, in arr [i]
+ *      by running the esieve algorithm serially.
+ *   2  Then run the esieve algorithm for numThread times, each time
+ *      increasing the number of threads by 1, starting from 1.
+ *   3  Prints the time taken to execute the program.
+ * */
+void checkPerformance(const big num, const int numThreads)
 {
-    char * arr = createArray(num);
+    /***************** Function Declarations ***********************/
+    
+    // Implements the seiving algorithm
+    void eseive (const big num, char * arr);
+
+    // Allocates an empty array of size num and initializes it to 0s
+    char *createArray (const big num);
+
+    // Deallocates the array pointed by arr.
+    void destroyArray (char * arr);
+
+    // Gets the current system time and returns it.
+    double getWallTime ();
+
+    /***************************************************************/
+    char * arr = createArray(num); /* Create an empty array.
+        * index i represents whether arr [i] is prime or not.
+        * if arr [i] == 0, it is not prime,
+        * else arr [i] is prime. */
+
     //Warm up cache
-    omp_set_num_threads(1);
+    omp_set_num_threads(1); // set number of threads for openmp
+    
     eseive(num, arr);
+    
+    // Actual perfomance evaluation.
     for(int i = 1; i <= numThreads; ++i)
     {
-	omp_set_num_threads(i);
-	const double startTime = getWallTime();
-	eseive(num, arr);
-	const double stopTime = getWallTime();
-	printf("Threads %d\tTime %g\n", i, stopTime-startTime);
+        omp_set_num_threads(i); // Set thread count
+        
+        const double startTime = getWallTime(); // get start time.
+        eseive (num, arr); // run algorithm
+        const double stopTime = getWallTime(); // get stop time.
+        
+        printf("Threads %d\tTime %g\n", i, stopTime-startTime); /*
+            * print the results. */
     }
-    destroyArray(arr);
+    
+    destroyArray(arr); // deallocate the array.
     return;
 }
 
-void printPrimes(const big num,
-	const char * arr)
+/* Input:
+ *      num: size of the array.
+ *      arr: pointer to the array containing primes.
+ * Output:
+ *      prints the primes in the array arr to stdout.
+ * */
+void printPrimes(const big num, const char * arr)
 {
+    if (arr == NULL)
+    {
+        printf ("printPrimes: array pointer is NULL.\n");
+        printf ("Fatal Error. Exiting.\n");
+        exit (1);
+    }
+    // else continue execution.
+    
     printf("Primes are :\n");
     for(big i = 0; i < num; ++i)
     {
-	if(!arr[i])
-	    printf("%lld\n", i);
+        if(arr[i] != 0)
+        {
+            // It is a prime number, print it.
+            printf("%lld\n", i);
+        }
+        else
+            ; // It is not a prime number skip it.
     }
     return;
 }
+
+/** Input: 
+ *      None.
+ *  Output:
+ *      The current system time.
+ * */
+double getWallTime ()
+{
+    struct timeval time;
+    if (gettimeofday (&time, NULL) != 0)
+    {
+        printf ("getWallTime: couldn't fetch system time. ");
+        printf ("Fatal error. Exiting.\n");
+        exit (1);;
+    }
+    // else continue execution.
+    
+    double wallTime;
+    wallTime = (double) time.tv_sec + (double) time.tv_usec * 0.000001;
+    return wallTime;
+}
+
+/* Input:
+ *  bits: Number of bits in the random number that will be genrated.
+ * Output:
+ *  A random number having "bits" number of bits.
+ * This Function:
+ *  For each bit:
+ *   1   genrates a random number.
+ *   2   takes its modulus with 2 to get 0 or 1, randomly.
+ *   3   left shifts the number, num, (the random number genrated so far
+ *       by 1.
+ *   4   Then sets the Right most digit as 0 or 1 based on the above
+ *       randomly genrated bit.
+ * */
+big createBigNumber (const int bits)
+{
+    big num = 0; // Initialize all bits to 0.
+    big thisBit; // Temporary variable used to store a bit in loop.
+
+    srand(time(NULL)); // Seed for rand ().
+    
+    // For each bit.
+    for(int i = 0; i < bits; ++i)
+    {
+        // Gerate a random bit (%2) and sdet it to the ith bit.
+        num     = num << 1; // left shift by 1.
+        
+        thisBit = (rand() % 2); /* Randomly get 0 or 1. All other bits
+            * except the least significant digit in thisBit are 0. 
+            * The least significant is either 0 or 1, randomly*/
+ 
+        num     |= thisBit; // Binary OR operation with thisBit
+    }
+    return num;
+}
+
+/** Input:  
+ *     num: size of the array.
+ *  Output:
+ *     returns pointer to an character array with each element have 
+ *     been initialized to 0.
+ *  This function:
+ *     dynamically allocates an array of size num.
+ * */
+char *createArray (const big num)
+{
+    char *arr = (char *) malloc (num * sizeof (char)); // Allocate.
+    memset (arr, 0, num * sizeof (char)); // Initialize
+    return arr;
+}
+
+/** Input:
+ *      arr: pointer to an array.
+ *  This Function:
+ *      Deallocates the memory pointed by arr from the process.
+ * */
+void destroyArray (char * arr)
+{
+    if (arr == NULL)
+    {
+        printf ("destroyArray: Error: Input pointer is NULL.\n");
+        return;
+    }
+	free (arr); // deallocate since arr is not NULL.
+    arr = NULL;
+}
+
